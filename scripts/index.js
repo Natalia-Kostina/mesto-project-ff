780987d64ad003daa0367ed5902d4d2bac30c215
Natalia Kostina <sazhin4natasha@yandex.ru>
2025-01-13 20:08:44 +0300

    Без правок

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,29 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardTemplate = document.querySelector('#card-template').content;
+const placesList = document.querySelector('.places__list');
+
+function createCards(card) {
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+
+    cardElement.querySelector('.card__image').src = card.link;
+    cardElement.querySelector('.card__image').alt = card.alt;
+    cardElement.querySelector('.card__title').textContent = card.name;
+
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', () => {
+        deleteButton.closest('.places__item').remove();
+    });
+
+    return cardElement;
+}
+
+function renderCard(cardElement) {
+    placesList.append(cardElement);
+}
+
+initialCards.forEach((card) => {
+    renderCard(createCards(card))
+});
